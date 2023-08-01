126b5d9598cfa5a27094c0afc3ac1a1a63ed90f5
guna-design <gunasekar20101999.com>
2023-08-02 00:17:57 +0530

    caps in a string array

--- script.js
+++ script.js
@@ -10,3 +10,12 @@ for(var i=1;i<=a;i++)
  }
 console.log(b);
 
+//2=>Convert all the strings to title caps in a string array 
+
+let a= userInput[0].split(" ");
+for (let i = 0; i < a.length; i++) {
+  let firstLetter = a[i].charAt(0).toUpperCase();
+  a[i] = firstLetter + a[i].slice(1);
+}
+console.log(a.join(" "));
+
