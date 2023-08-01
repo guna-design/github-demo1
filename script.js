454ee95d57f3deb0e4521f196c46021f58c3c1c7
guna-design <gunasekar20101999.com>
2023-08-01 21:11:54 +0530

    odd numbers in array

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-let a =+userInput[0];
-for(i=1;i<=)
+//1=> Print odd numbers in array
+
+var a=+userInput[0];
+var b=[];
+for(var i=1;i<=a;i++)
+{
+    if (i%2!=0){
+        b.push(i);
+    }
+ }
+console.log(b);
+
